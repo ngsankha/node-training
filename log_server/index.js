6562bdb027a6e8e4afd53c02d2ae05006c5b4300
Sankha Narayan Guria <sankha93@gmail.com>
2015-06-15 18:30:26 +0530

    send only 10 lines at the start

--- log_server/index.js
+++ log_server/index.js
@@ -1,7 +1,9 @@
 var app = require('express')(),
     http = require('http').Server(app),
     io = require('socket.io')(http),
-    fs = require('fs');
+    fs = require('fs'),
+    byline = require('byline'),
+    os = require('os');
 
 if (process.argv.length < 4) {
   console.log("Usage: node index.js [filename] [port]");
@@ -11,23 +13,33 @@ if (process.argv.length < 4) {
 var filename = process.argv[2],
     port = process.argv[3];
 
-var data = fs.readFileSync(filename).toString();
-fs.watch(process.argv[2], function(event, fname) {
+var lines10 = [];
+var addLine = function(line) {
+  lines10.push(line);
+  if (lines10.length > 10)
+    lines10.shift();
+};
+
+var stream = byline(fs.createReadStream(filename));
+var endLength = 0;
+stream.on('data', function(line) {
+  line += os.EOL;
+  addLine(line);
+  endLength += line.length;
+});
+
+fs.watch(filename, function(event, fname) {
   if (event == 'change') {
-    var inStream = fs.createReadStream(filename, { start: data.length });
+    var inStream = byline(fs.createReadStream(filename, { start: endLength }));
     var text = '';
-    inStream.on('data', function(chunk) {
-      text += chunk.toString();
-    });
-    inStream.on('end', function() {
-      data += text;
-      io.emit('chunk', text);
+    inStream.on('data', function(line) {
+      line += os.EOL;
+      addLine(line.toString());
+      endLength += line.length;
+      io.emit('chunk', line.toString());
     });
   }
 });
-var inStream = fs.createReadStream(process.argv[2], { autoClose: false });
-var text = "";
-
 
 app.get('/', function(req, res) {
   res.send("log server");
@@ -39,9 +51,9 @@ app.get('/log', function(req, res) {
 
 io.on('connection', function(socket) {
   console.log("User connected: Sending entire log");
-  io.emit('initial log', data);
+  io.emit('initial log', lines10.join(""));
 });
 
-http.listen(process.argv[3], function() {
-  console.log("Listening on *:" + process.argv[3]);
+http.listen(port, function() {
+  console.log("Listening on *:" + port);
 });
